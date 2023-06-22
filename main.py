88da5118f37651a6e36a9150abdbaf0652db9c7e
An Jaebeom <ajb8533296@gmail.com>
2023-06-22 17:26:27 +0900

    버전 추가

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from modules.download import Download
 
+version = "1.0.0"
+
+print("SECRAFT INSTALLER")
+print("MADE BY AJB")
+print(f"Version: {version}")
+
 download = Download()
 download.setup()
